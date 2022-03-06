a5390ce25bd90149bf57a46c61979d48c3e7bbe9
christine <chrisitne.brown@bhcc.edu>
2022-03-06 13:39:46 -0500

    Initial commit

---
+++ src/test/java/com/careerdevs/jsonplaceholder1/Jsonplaceholder1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.careerdevs.jsonplaceholder1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsonplaceholder1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
